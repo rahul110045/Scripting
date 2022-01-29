421aa2fdad3e23b99460d4056494ca093fd1b29f
rkumar624 <rahul.kumar2@dxc.com>
2022-01-29 22:46:44 +0530

    added

---
+++ argument.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo "number of argument: $#"
+echo "script name: $0"
+echo "first argument: $1"
+echo "second argument: $2"
+echo "third argument: $3"
+echo "fourth argument: $4"
+echo "fifth argument: $5"
+echo "sixth argument: $6"
